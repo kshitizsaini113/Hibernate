c2ab6d04def3fec69f5d5880a47a202c0390939a
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2023-11-20 19:30:44 +0530

    select in HQL

---
+++ src/main/java/com/kshitizsaini113/Hibernate010/Main.java
@@ -0,0 +1,30 @@
+package com.kshitizsaini113.Hibernate010;
+
+import com.kshitizsaini113.Entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        String queryString1 = "from Student";
+        Query<Student> query1 = session.createQuery(queryString1, Student.class);
+
+        List<Student> list1 = query1.list();
+
+        for(Student stu: list1) {
+            System.out.println(stu);
+        }
+
+        session.close();
+        sessionFactory.close();
+    }
+}
